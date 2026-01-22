56942e0f485d55fc1e96dd0cabbf3800fbd7e63b
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2026-01-22 22:30:45 +0530

    Add deleteLast function to calculator

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -14,3 +14,13 @@ function calculate() {
   document.getElementById("expr").value = expression;
   document.getElementById("calcForm").submit();
 }
+
+function deleteLast() {
+  expression = expression.slice(0, -1);
+
+  if (expression === "") {
+    document.getElementById("screen").innerText = "0";
+  } else {
+    document.getElementById("screen").innerText = expression;
+  }
+}
